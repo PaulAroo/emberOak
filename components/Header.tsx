0edf23c2c892d49dd1531d2d8c308422cb789058
PaulAroo <timmypaul2006@gmail.com>
2024-03-17 22:01:37 +0100

    wip: user dropdown

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import { Search, ShoppingCart } from "lucide-react"
 
 import { useUser } from "@/hooks/useUser"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export function Header() {
 	const user = useUser()
@@ -12,13 +13,18 @@ export function Header() {
 		<header className="py-2 border-b">
 			<div className="container flex justify-between">
 				<Link href="/">EmberOak</Link>
-				<nav className="flex gap-4">
-					<Search />
+				<nav className="flex gap-4 items-center">
 					<Link href="/products">Products</Link>
+					<Search />
 					{user ? (
 						<>
-							<p>{user.name}</p>
 							<ShoppingCart />
+							<Avatar>
+								<AvatarImage src="" alt="" />
+								<AvatarFallback>
+									{user.name?.slice(0, 2).toLocaleUpperCase()}
+								</AvatarFallback>
+							</Avatar>
 						</>
 					) : (
 						<Link href="/signin">Login</Link>
